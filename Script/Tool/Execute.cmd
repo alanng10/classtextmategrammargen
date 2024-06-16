59a55d0b9d60d1243d97a7063208da703b963501
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-06-16 14:39:06 +0800

    Script

--- Script/Tool/Execute.cmd
+++ Script/Tool/Execute.cmd
@@ -1,8 +1,9 @@
 @echo off
 
 set ToolName=%~1
+set Arg0=%~2
 set ModuleName=Z.Tool.%ToolName%
 pushd Out\net8.0
-dotnet %ModuleName%.dll
+dotnet %ModuleName%.dll %Arg0%
 echo Status: %errorlevel%
 popd
